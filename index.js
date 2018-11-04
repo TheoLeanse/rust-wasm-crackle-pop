fc0ecf003a30a99cd4ad2bff5fb196f1a5d01f6f
theoleanse <theoleanse@gmail.com>
2018-11-04 18:42:27 +0000

    do it til you're satisfied

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const crackle_pop = import('./rust_wasm_crackle_pop');
+const init = async () => {
+	const cp = await import('./rust_wasm_crackle_pop');
+	const result = Array.from(Array(100).keys())
+		.map(x => x + 1)
+		.map(x => cp.get_result(x));
+	console.log(result);
+};
 
-crackle_pop.then(cp => {
-	console.log(cp.get_result(100));
-});
+init();
